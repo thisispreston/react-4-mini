1f5e860595b5a7c27e652fa5021d4807bf38b7b8
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-02-13 11:59:38 -0700

    Yatta

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
+import routes from './routes'
+import {HashRouter as Router} from 'react-router-dom'
 import Nav from "./Components/Nav/Nav";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Nav />
-        <h1 style={{ padding: "200px 35%" }}>
-          This is where your pages will appear
-        </h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Nav />
+          {routes}
+        </div>
+      </Router>
     );
   }
 }
